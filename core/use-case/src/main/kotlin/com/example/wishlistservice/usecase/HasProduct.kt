49522e86fd661e0efcc534c075d336673c5bbb92
Gabriel Machado <gabrielmachado616@hotmail.com>
2023-09-24 10:09:48 -0300

    Has product use case

---
+++ core/use-case/src/main/kotlin/com/example/wishlistservice/usecase/HasProduct.kt
@@ -0,0 +1,16 @@
+package com.example.wishlistservice.usecase
+
+import com.example.wishlistservice.usecase.repository.WishlistRepository
+
+class HasProduct(
+    private val wishlistRepository: WishlistRepository
+) {
+
+    fun customerHasProductOnWishlist(customerId: String, productId: String): Boolean {
+        return wishlistRepository
+            .findByCustomerId(customerId)
+            ?.productIds
+            ?.contains(productId) != null
+    }
+
+}
---
+++ core/use-case/src/test/kotlin/com/example/wishlistservice/usecase/HasProductTest.kt
@@ -0,0 +1,35 @@
+package com.example.wishlistservice.usecase
+
+import com.example.wishlistservice.domain.Wishlist
+import com.example.wishlistservice.usecase.repository.WishlistRepository
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class HasProductTest() {
+
+    private val wishlistRepository = mockk<WishlistRepository>()
+
+    @Test
+    fun `Should return false when there is no wishlist for customer`() {
+        every { wishlistRepository.findByCustomerId(any()) } returns null
+        assertFalse(
+            HasProduct(wishlistRepository)
+                .customerHasProductOnWishlist("Customer", "Product")
+        )
+    }
+
+    @Test
+    fun `Should return true when there is no wishlist for customer`() {
+        val product = "Product"
+        val wishlist = Wishlist("Customer", setOf(product))
+        every { wishlistRepository.findByCustomerId(any()) } returns wishlist
+        assertTrue(
+            HasProduct(wishlistRepository)
+                .customerHasProductOnWishlist(wishlist.customerId, product)
+        )
+    }
+
+}
