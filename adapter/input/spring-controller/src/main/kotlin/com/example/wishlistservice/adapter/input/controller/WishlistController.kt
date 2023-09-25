9b43a6b5e175b1da134524a725fa1aaf2c6347b0
Gabriel Machado <gabrielmachado616@hotmail.com>
2023-09-25 13:26:33 -0300

    Swagger details

--- adapter/input/spring-controller/src/main/kotlin/com/example/wishlistservice/adapter/input/controller/WishlistController.kt
+++ adapter/input/spring-controller/src/main/kotlin/com/example/wishlistservice/adapter/input/controller/WishlistController.kt
@@ -1,6 +1,8 @@
 package com.example.wishlistservice.adapter.input.controller
 
+import com.example.wishlistservice.adapter.input.dto.ErrorDTO
 import com.example.wishlistservice.adapter.input.dto.ProductWishlistedDTO
+import com.example.wishlistservice.adapter.input.dto.WishlistDTO
 import com.example.wishlistservice.adapter.input.mapper.WishlistDTOMapper
 import com.example.wishlistservice.domain.WishlistProductLimitExceededException
 import com.example.wishlistservice.usecase.AddProduct
@@ -8,12 +10,20 @@ import com.example.wishlistservice.usecase.GetProducts
 import com.example.wishlistservice.usecase.HasProduct
 import com.example.wishlistservice.usecase.RemoveProduct
 import com.example.wishlistservice.usecase.exception.WishlistProductNotFoundException
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/wishlist")
+@RequestMapping("/wishlist", produces = ["application/json"])
+@Tag(name = "Wishlist API")
 class WishlistController(
     private val addProduct: AddProduct,
     private val removeProduct: RemoveProduct,
@@ -22,6 +32,19 @@ class WishlistController(
     private val wishlistDTOMapper: WishlistDTOMapper
 ) {
 
+    @Operation(summary = "Add a product to customer's wishlist")
+    @ApiResponses(value = [
+        ApiResponse(
+            responseCode = "200",
+            description = "Returns the complete wishlist",
+            content = [Content(mediaType = "application/json", schema = Schema(implementation = WishlistDTO::class))]
+        ),
+        ApiResponse(
+            responseCode = "422",
+            description = "When wishlist would have more than 20 products",
+            content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorDTO::class))]
+        )
+    ])
     @PutMapping("/customer/{customerId}/product/{productId}")
     fun addProduct(@PathVariable customerId: String, @PathVariable productId: String): ResponseEntity<*> {
         return try {
@@ -35,6 +58,19 @@ class WishlistController(
         }
     }
 
+    @Operation(summary = "Remove a product from customer's wishlist")
+    @ApiResponses(value = [
+        ApiResponse(
+            responseCode = "204",
+            description = "Product removed",
+            content = [Content(mediaType = "application/json", schema = Schema(implementation = Void::class))]
+        ),
+        ApiResponse(
+            responseCode = "404",
+            description = "When given product not on customer's wishlist",
+            content = [Content(mediaType = "application/json", schema = Schema(implementation = ErrorDTO::class))]
+        )
+    ])
     @DeleteMapping("/customer/{customerId}/product/{productId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun removeProduct(@PathVariable customerId: String, @PathVariable productId: String): ResponseEntity<*> {
@@ -48,10 +84,33 @@ class WishlistController(
        }
     }
 
+
+    @Operation(summary = "Get products from customer's wishlist")
+    @ApiResponses(value = [
+        ApiResponse(
+            responseCode = "200",
+            description = "Product list",
+            content = [Content(
+                mediaType = "application/json",
+                array = ArraySchema(schema = Schema(implementation = String::class))
+            )]
+        )
+    ])
     @GetMapping("/customer/{customerId}")
     fun getProducts(@PathVariable customerId: String): Set<String> = getProducts
         .getProducts(customerId = customerId)
 
+    @Operation(summary = "Get whether a product is wishlisted")
+    @ApiResponses(value = [
+        ApiResponse(
+            responseCode = "200",
+            description = "Whether given product is wishlisted",
+            content = [Content(
+                mediaType = "application/json",
+                schema = Schema(implementation = ProductWishlistedDTO::class)
+            )]
+        )
+    ])
     @GetMapping("/customer/{customerId}/product/{productId}")
     fun hasProduct(@PathVariable customerId: String, @PathVariable productId: String): ProductWishlistedDTO = hasProduct
         .customerHasProductOnWishlist(customerId = customerId, productId = productId)
